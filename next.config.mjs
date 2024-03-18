88cd46ff97c3cc9e8fba75706c64f16bfd075feb
y36jung <youhan.jung@gmail.com>
2024-03-18 01:05:29 -0400

    fix 9

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  //This is for enabling static imports for GitHub Actions
+  output:'export',
   reactStrictMode: false,
 };
-//module.exports = nextConfig
+/* module.exports = nextConfig*/
 export default nextConfig;

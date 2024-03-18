b5d1ed8b6cc1704340a4363a2c6e7773a67d99df
y36jung <youhan.jung@gmail.com>
2024-03-18 00:39:08 -0400

    commented reactStrictMode

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  //This is for enabling static imports for GitHub Actions
-  reactStrictMode: false,
+  //reactStrictMode: false,
 };
-
+/* module.exports = nextConfig*/
 export default nextConfig;
